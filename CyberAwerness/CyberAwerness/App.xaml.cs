f465d06fe86a105c269261bf325f889716af6dd2
Ayanda Skosana <67970674+sinnistar16@users.noreply.github.com>
2025-06-27 15:32:38 +0200

    Version 3

---
+++ CyberAwerness/CyberAwerness/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace CyberChatBotWPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Center the window on screen
+            var mainWindow = new MainWindow();
+            mainWindow.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            mainWindow.Show();
+        }
+    }
+}
